e85a90af96d5877c4eae55c1cd3391e9b20d2a0c
Walter Medvedeo <wmedvedeo@gmail.com>
2023-12-18 18:30:17 +0100

    initial version

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,18 @@
+package org.acme;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.time.ZonedDateTime;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy: " + ZonedDateTime.now();
+    }
+}
